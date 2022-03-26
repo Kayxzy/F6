3ad6cdcd5f9e5c965d1e1ed195c20fead0249da1
mrismanaziz <mrismanaziz@gmail.com>
2022-03-26 14:41:06 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -11,6 +11,16 @@ from config import CHANNEL, GROUP, OWNER
 from Data import Data
 
 
+@Bot.on_message(filters.private & filters.incoming & filters.command("about"))
+async def _about(client: Bot, msg: Message):
+    await client.send_message(
+        msg.chat.id,
+        Data.ABOUT,
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(Data.close),
+    )
+
+
 @Bot.on_message(filters.private & filters.incoming & filters.command("help"))
 async def _help(client: Bot, msg: Message):
     await client.send_message(
@@ -28,9 +38,20 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             await query.message.edit_text(
                 text=f"<b>Tentang Bot ini:\n\n • Owner: @{OWNER}\n • Channel: @{CHANNEL}\n • Group: @{GROUP}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>Klik Disini</a>\n • Owner Repo: @mrismanaziz</b>\n",
                 disable_web_page_preview=True,
-                reply_markup=InlineKeyboardMarkup(
-                    [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
-                ),
+                reply_markup=InlineKeyboardMarkup(Data.close),
+            )
+        except MessageNotModified:
+            pass
+    elif data == "help":
+        try:
+            chat_id = query.from_user.id
+            message_id = query.message.message_id
+            await query.message.edit_text(
+                chat_id=chat_id,
+                message_id=message_id,
+                text=Data.HELP,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(Data.close),
             )
         except MessageNotModified:
             pass
