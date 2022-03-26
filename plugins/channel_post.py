ccadd111b60009a4da2d850bf86a400898ca2a5c
mrismanaziz <mrismanaziz@gmail.com>
2022-03-26 14:32:15 +0700

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -17,7 +17,7 @@ from helper_func import encode
     filters.private
     & filters.user(ADMINS)
     & ~filters.command(
-        ["start", "users", "broadcast", "ping", "uptime", "batch", "genlink", "help"]
+        ["about", "batch", "broadcast", "genlink", "help", "ping", "start", "uptime", "users"]
     )
 )
 async def channel_post(client: Client, message: Message):
