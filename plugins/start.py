d22f81ec86e4499c5c5dadf48602cbcfae83c15c
mrismanaziz <mrismanaziz@gmail.com>
2022-03-28 04:05:06 +0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -3,8 +3,6 @@
 # t.me/SharingUserbot & t.me/Lunatic0de
 
 import asyncio
-from datetime import datetime
-from time import time
 
 from bot import Bot
 from config import (
@@ -15,9 +13,9 @@ from config import (
     PROTECT,
     START_MSG,
 )
-from database.sql import add_user, full_userbase, query_msg
+from database.sql import add_user, full_userbase
 from pyrogram import filters
-from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked
+from pyrogram.errors import FloodWait
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 from helper_func import (
@@ -33,27 +31,6 @@ from helper_func import (
 )
 
 from .button import fsub_button, start_button
-
-START_TIME = datetime.utcnow()
-START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
-TIME_DURATION_UNITS = (
-    ("week", 60 * 60 * 24 * 7),
-    ("day", 60**2 * 24),
-    ("hour", 60**2),
-    ("min", 60),
-    ("sec", 1),
-)
-
-
-async def _human_time_duration(seconds):
-    if seconds == 0:
-        return "inf"
-    parts = []
-    for unit, div in TIME_DURATION_UNITS:
-        amount, seconds = divmod(int(seconds), div)
-        if amount > 0:
-            parts.append("{} {}{}".format(amount, unit, "" if amount == 1 else "s"))
-    return ", ".join(parts)
 
 
 @Bot.on_message(
@@ -188,76 +165,3 @@ async def get_users(client: Bot, message: Message):
     )
     users = await full_userbase()
     await msg.edit(f"{len(users)} <b>Pengguna menggunakan bot ini</b>")
-
-
-@Bot.on_message(filters.command("broadcast") & filters.user(ADMINS))
-async def send_text(client: Bot, message: Message):
-    if message.reply_to_message:
-        query = await query_msg()
-        broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
-
-        pls_wait = await message.reply(
-            "<code>Broadcasting Message Tunggu Sebentar...</code>"
-        )
-        for row in query:
-            chat_id = int(row[0])
-            if chat_id not in ADMINS:
-                try:
-                    await broadcast_msg.copy(chat_id, protect_content=PROTECT)
-                    successful += 1
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
-                    await broadcast_msg.copy(chat_id, protect_content=PROTECT)
-                    successful += 1
-                except UserIsBlocked:
-                    blocked += 1
-                except InputUserDeactivated:
-                    deleted += 1
-                except BaseException:
-                    unsuccessful += 1
-                total += 1
-        status = f"""<b><u>Berhasil Broadcast</u>
-Jumlah Pengguna: <code>{total}</code>
-Berhasil: <code>{successful}</code>
-Gagal: <code>{unsuccessful}</code>
-Pengguna diblokir: <code>{blocked}</code>
-Akun Terhapus: <code>{deleted}</code></b>"""
-        return await pls_wait.edit(status)
-    else:
-        msg = await message.reply(
-            "<code>Gunakan Perintah ini Harus Sambil Reply ke pesan telegram yang ingin di Broadcast.</code>"
-        )
-        await asyncio.sleep(8)
-        await msg.delete()
-
-
-@Bot.on_message(filters.command("ping"))
-async def ping_pong(client, m: Message):
-    start = time()
-    current_time = datetime.utcnow()
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
-    m_reply = await m.reply_text("Pinging...")
-    delta_ping = time() - start
-    await m_reply.edit_text(
-        "<b>PONG!!</b>🏓 \n"
-        f"<b>• Pinger -</b> <code>{delta_ping * 1000:.3f}ms</code>\n"
-        f"<b>• Uptime -</b> <code>{uptime}</code>\n"
-    )
-
-
-@Bot.on_message(filters.command("uptime"))
-async def get_uptime(client, m: Message):
-    current_time = datetime.utcnow()
-    uptime_sec = (current_time - START_TIME).total_seconds()
-    uptime = await _human_time_duration(int(uptime_sec))
-    await m.reply_text(
-        "🤖 <b>Bot Status:</b>\n"
-        f"• <b>Uptime:</b> <code>{uptime}</code>\n"
-        f"• <b>Start Time:</b> <code>{START_TIME_ISO}</code>"
-    )
