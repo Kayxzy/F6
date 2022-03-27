51540b948d3eeccef1c44cefb197090c06766cce
mrismanaziz <mrismanaziz@gmail.com>
2022-03-28 01:26:52 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -10,8 +10,8 @@ def start_button(client):
     if not FORCE_SUB_1 and not FORCE_SUB_2:
         buttons = [
             [
-                InlineKeyboardButton(text="• ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs •", callback_data="help"),
-                InlineKeyboardButton(text="• ᴛᴜᴛᴜᴘ •", callback_data="close"),
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
             ],
         ]
         return buttons
@@ -21,8 +21,8 @@ def start_button(client):
                 InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
             ],
             [
-                InlineKeyboardButton(text="• ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs •", callback_data="help"),
-                InlineKeyboardButton(text="• ᴛᴜᴛᴜᴘ •", callback_data="close"),
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
             ],
         ]
         return buttons
@@ -32,21 +32,21 @@ def start_button(client):
                 InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
             ],
             [
-                InlineKeyboardButton(text="• ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs •", callback_data="help"),
-                InlineKeyboardButton(text="• ᴛᴜᴛᴜᴘ •", callback_data="close"),
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
             ],
         ]
         return buttons
     if FORCE_SUB_1 and FORCE_SUB_2:
         buttons = [
             [
-                InlineKeyboardButton(text="• ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs •", callback_data="help"),
+                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
             ],
             [
                 InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
                 InlineKeyboardButton(text="ɢʀᴏᴜᴘ", url=client.invitelink2),
             ],
-            [InlineKeyboardButton(text="• ᴛᴜᴛᴜᴘ •", callback_data="close")],
+            [InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close")],
         ]
         return buttons
 
